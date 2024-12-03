8ce2c19856f5bd1e9a581de36819fd444f057c26
buithaison17 <125809724+buithaison17@users.noreply.github.com>
2024-12-03 10:28:23 +0700

    Add files via upload

---
+++ baitap10.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(){
+	int row, col;
+	printf("nhap so hang: ");
+	scanf("%d", &row);
+	printf("nhap so cot: ");
+	scanf("%d", &col);
+	int arr[row][col], numbers[row];
+	for(int i = 0; i < row; i++){
+		for(int j = 0; j < col; j++){
+			printf("nhap phan tu arr[%d][%d]: ", i, j);
+			scanf("%d", &arr[i][j]);
+		}
+	}
+	int j = col -1, n = 0;
+	for(int i = 0; i < row; i++){
+		numbers[n] = arr[i][j];
+		j--;
+		n++;
+	}
+	for(int i = 0; i < row; i++){
+		for(int j = 0; j < col - i -1; j++){
+			if(numbers[j] < numbers[j+1]){
+				int temp = numbers[j];
+				numbers[j] = numbers[j+1];
+				numbers[j+1] = temp;
+			}
+		}
+	}
+	for(int i = 0; i < row; i++){
+		printf("%d ", numbers[i]);
+	}
+	return 0;
+}
